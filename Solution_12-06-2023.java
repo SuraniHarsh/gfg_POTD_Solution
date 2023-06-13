990e6e878590de8b39ca623ec53a8d002e0d8279
SuraniHarsh <harshsurani00@gmail.com>
2023-06-13 22:43:20 +0530

    GFG day_5_13-06-2023 DSA

---
+++ Solution_12-06-2023.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class RodCutting {
+
+    public static void main(String args[])throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
+
+            Solution ob = new Solution();
+            out.println(ob.cutRod(arr, n));
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    public int cutRod(int price[], int n) {
+        int[] dp = new int[n+1];
+
+        for (int length = 1; length <= n; length++) {
+            int max_value = 0;
+            for (int i = 1; i <= length; i++) {
+                max_value = Math.max(max_value, price[i-1] + dp[length - i]);
+            }
+            dp[length] = max_value;
+        }
+
+        return dp[n];
+    }
+}
